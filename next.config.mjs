1e4938fbc1a2884aab2f86ef7d471912f0721af7
FIRST_NAME LAST_NAME <l.frz65@gmail.com>
2025-07-16 04:14:59 +0200

    config

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        remotePatterns: [
-            {
-                protocol: 'https',
-                hostname: 'https://airbnb-clone-2-957822787686.europe-west9.run.app',
-                pathname: '/**'
-            }
-        ]
-    }
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com", // ← pour les images Cloudinary
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "airbnb-clone-2-957822787686.europe-west9.run.app", // ← sans "https://"
+        pathname: "/**",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
